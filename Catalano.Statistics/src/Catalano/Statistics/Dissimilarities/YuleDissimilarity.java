b6f4e76386008022ddd82346c93307508fc390d7
Marcos Diego Catalano <diego.catalano@live.com>
2015-09-20 13:37:04 -0300

    CF 1.5
    Refactoring dissimilarity.

---
+++ Catalano.Statistics/src/Catalano/Statistics/Dissimilarities/YuleDissimilarity.java
@@ -0,0 +1,41 @@
+// Catalano Statistics Library
+// The Catalano Framework
+//
+// Copyright © Diego Catalano, 2015
+// diego.catalano at live.com
+//
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+package Catalano.Statistics.Dissimilarities;
+
+/**
+ * Yule dissimilarity.
+ * @author Diego Catalano
+ */
+public class YuleDissimilarity implements IDissimilarity<int[]>{
+
+    /**
+     * Initializes a new instance of the YuleDissimilarity class.
+     */
+    public YuleDissimilarity() {}
+
+    @Override
+    public double Compute(int[] u, int[] v) {
+        return Dissimilarity.Yule(u, v);
+    }
+    
+}
