58a7689664201c5f674f4bd2957b0075b749222e
Diego Catalano <diego.catalano@live.com>
2016-04-17 03:22:35 -0300

    CF 1.6
    - New: Grayscale to RGB.

---
+++ Catalano.Image/src/Catalano/Imaging/Filters/GrayscaleToRGB.java
@@ -0,0 +1,45 @@
+// Catalano Imaging Library
+// The Catalano Framework
+//
+// Copyright © Diego Catalano, 2015
+// diego.catalano at live.com
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+package Catalano.Imaging.Filters;
+
+import Catalano.Imaging.FastBitmap;
+import Catalano.Imaging.IApplyInPlace;
+
+/**
+ * Convert grayscale image in RGB space.
+ * @author Diego Catalano
+ */
+public class GrayscaleToRGB implements IApplyInPlace{
+
+    /**
+     * Initializes a new instance of the GrayscaleToRGB class.
+     */
+    public GrayscaleToRGB() {}
+
+    @Override
+    public void applyInPlace(FastBitmap fastBitmap) {
+        if(!fastBitmap.isGrayscale())
+            throw new IllegalArgumentException("GrayscaleToRGB only works in grayscale images.");
+        
+        fastBitmap.toRGB();   
+    }   
+}
--- Catalano.Image/src/Catalano/Imaging/Filters/Photometric/SelfQuocientImage.java
+++ Catalano.Image/src/Catalano/Imaging/Filters/Photometric/SelfQuocientImage.java
@@ -6,6 +6,10 @@
 package Catalano.Imaging.Filters.Photometric;
 
 import Catalano.Imaging.FastBitmap;
+import Catalano.Imaging.Tools.ImageUtils;
+import Catalano.Imaging.Tools.Kernel;
+import Catalano.Math.Functions.Gaussian;
+import Catalano.Math.PaddingMatrix;
 
 /**
  *
@@ -15,6 +19,7 @@ public class SelfQuocientImage implements IPhotometricFilter{
     
     private int size;
     private double sigma;
+    private double[][] vectors;
 
     public double getSigma() {
         return sigma;
@@ -22,6 +27,7 @@ public class SelfQuocientImage implements IPhotometricFilter{
 
     public void setSigma(double sigma) {
         this.sigma = sigma;
+        BuildKernel();
     }
 
     public int getSize() {
@@ -30,6 +36,7 @@ public class SelfQuocientImage implements IPhotometricFilter{
 
     public void setSize(int size) {
         this.size = size;
+        BuildKernel();
     }
 
     public SelfQuocientImage() {
@@ -39,12 +46,40 @@ public class SelfQuocientImage implements IPhotometricFilter{
     public SelfQuocientImage(int size, double sigma) {
         this.size = size;
         this.sigma = sigma;
+        BuildKernel();
+    }
+    
+    private void BuildKernel(){
+        Gaussian g = new Gaussian(sigma);
+        this.vectors = Kernel.Decompose(g.Kernel2D(size));
     }
 
     @Override
     public void applyInPlace(FastBitmap fastBitmap) {
         if(!fastBitmap.isGrayscale())
             throw new IllegalArgumentException("Self Quocient Image only works in grayscale images.");
+        
+        double[][] image = fastBitmap.toMatrixGrayAsDouble();
+        
+        double[][] result = Process(image, true);
+        
+        
+        
+        
+    }
+    
+    public double[][] Process(double[][] image, boolean normalize){
+        
+        //Normalize the image
+        ImageUtils.Normalize(image);
+        
+        //Apply gaussian smoothing
+        double[][] filt1 = ImageUtils.Convolution(image, vectors[0], vectors[1]);
+        
+        //Image padding
+        int padSize = (int)Math.floor(size/2.0);
+        PaddingMatrix pad = new PaddingMatrix(padSize, padSize, true);
+        double[][] padX = pad.Create(image);
         
         
     }
