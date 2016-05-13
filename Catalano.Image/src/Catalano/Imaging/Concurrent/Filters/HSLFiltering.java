6cc1146e5540bbb3e1fb86baa6d91a2228878ad9
Diego Catalano <diego.catalano@live.com>
2016-05-13 18:20:07 -0300

    Fixing some color conversions
    - Manually changed from arantius

--- Catalano.Image/src/Catalano/Imaging/Concurrent/Filters/HSLFiltering.java
+++ Catalano.Image/src/Catalano/Imaging/Concurrent/Filters/HSLFiltering.java
@@ -258,7 +258,7 @@ public class HSLFiltering implements IApplyInPlace{
                     int g = share.fastBitmap.getGreen(i, j);
                     int b = share.fastBitmap.getBlue(i, j);
                     
-                    float[] hsl = ColorConverter.RGBtoHSL(r, g, b);
+                    double[] hsl = ColorConverter.RGBtoHSL(r, g, b);
                     
                     // check HSL values
                     if (
