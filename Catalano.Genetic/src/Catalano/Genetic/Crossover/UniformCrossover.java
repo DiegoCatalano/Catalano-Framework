22deecabb08518ec1e6bcf011d95a69703e601c7
Diego Catalano <diego.catalano@live.com>
2017-02-11 17:59:25 -0200

    Reworking uniform crossover
    - Reworked: Uniform crossover now supports generic chromossomes

--- Catalano.Genetic/src/Catalano/Genetic/Crossover/UniformCrossover.java
+++ Catalano.Genetic/src/Catalano/Genetic/Crossover/UniformCrossover.java
@@ -7,6 +7,7 @@
 package Catalano.Genetic.Crossover;
 
 import Catalano.Genetic.BinaryChromosome;
+import Catalano.Genetic.IChromosome;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,30 +15,39 @@ import java.util.List;
  *
  * @author Diego
  */
-public class UniformCrossover implements ICrossover<BinaryChromosome>{
+public class UniformCrossover implements ICrossover<IChromosome>{
 
     public UniformCrossover() {}
 
     @Override
-    public List<BinaryChromosome> Compute(BinaryChromosome chromosome1, BinaryChromosome chromosome2) {
+    public List<IChromosome> Compute(IChromosome chromosome1, IChromosome chromosome2) {
         
-        String c1 = "";
-        String c2 = "";
+        List<Object> c1 = new ArrayList<Object>(chromosome1.getLength());
+        List<Object> c2 = new ArrayList<Object>(chromosome1.getLength());
+        
         int size = chromosome1.getLength();
         for (int i = 0; i < size; i++) {
             if(Math.random() <= 0.5){
-                c1 = chromosome1.getGene(i).toString();
-                c2 = chromosome2.getGene(i).toString();
+                c1.add(chromosome1.getGene(i));
+                c2.add(chromosome2.getGene(i));
             }
             else{
-                c1 = chromosome2.getGene(i).toString();
-                c2 = chromosome1.getGene(i).toString();
+                c1.add(chromosome2.getGene(i));                
+                c2.add(chromosome1.getGene(i));
             }
         }
         
-        List<BinaryChromosome> lst = new ArrayList<BinaryChromosome>(2);
-        lst.add(new BinaryChromosome(size, c1));
-        lst.add(new BinaryChromosome(size, c2));
+        IChromosome cc1 = chromosome1.Clone();
+        IChromosome cc2 = chromosome2.Clone();
+        
+        for (int i = 0; i < cc1.getLength(); i++) {
+            cc1.setGene(i, c1.get(i));
+            cc2.setGene(i, c2.get(i));
+        }
+        
+        List<IChromosome> lst = new ArrayList<IChromosome>(2);
+        lst.add(cc1);
+        lst.add(cc2);
         
         return lst;
         
