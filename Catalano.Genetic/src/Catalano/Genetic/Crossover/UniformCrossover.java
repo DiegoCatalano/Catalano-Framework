dd75e64b1ccb090af24da66414eb4be85590c6bd
Diego Catalano <diego.catalano@live.com>
2019-04-25 13:21:36 -0300

    Integer Chromosome
    - New: Integer Chromosome

--- Catalano.Genetic/src/Catalano/Genetic/Crossover/UniformCrossover.java
+++ Catalano.Genetic/src/Catalano/Genetic/Crossover/UniformCrossover.java
@@ -21,32 +21,24 @@ public class UniformCrossover implements ICrossover<IChromosome>{
     @Override
     public List<IChromosome> Compute(IChromosome chromosome1, IChromosome chromosome2) {
         
-        List<Object> c1 = new ArrayList<Object>(chromosome1.getLength());
-        List<Object> c2 = new ArrayList<Object>(chromosome1.getLength());
+        IChromosome c1 = chromosome1.Clone();
+        IChromosome c2 = chromosome2.Clone();
         
         int size = chromosome1.getLength();
         for (int i = 0; i < size; i++) {
             if(Math.random() <= 0.5){
-                c1.add(chromosome1.getGene(i));
-                c2.add(chromosome2.getGene(i));
+                c1.setGene(i, chromosome1.getGene(i));
+                c2.setGene(i, chromosome2.getGene(i));
             }
             else{
-                c1.add(chromosome2.getGene(i));                
-                c2.add(chromosome1.getGene(i));
+                c1.setGene(i, chromosome2.getGene(i));                
+                c2.setGene(i, chromosome1.getGene(i));
             }
-        }
-        
-        IChromosome cc1 = chromosome1.Clone();
-        IChromosome cc2 = chromosome2.Clone();
-        
-        for (int i = 0; i < cc1.getLength(); i++) {
-            cc1.setGene(i, c1.get(i));
-            cc2.setGene(i, c2.get(i));
         }
         
         List<IChromosome> lst = new ArrayList<IChromosome>(2);
-        lst.add(cc1);
-        lst.add(cc2);
+        lst.add(c1);
+        lst.add(c2);
         
         return lst;
         
---
+++ Catalano.Genetic/src/Catalano/Genetic/IntegerChromosome.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Catalano.Genetic;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Diego Catalano
+ */
+public class IntegerChromosome extends ChromosomeBase{
+    
+    private int size;
+    private int maxValue;
+    
+    private int[] values;
+
+    public int getMaxValue() {
+        return maxValue;
+    }
+
+    public IntegerChromosome(int size, int maxValue) {
+        this.size = size;
+        this.maxValue = maxValue;
+        Generate();
+    }
+
+    @Override
+    public Object getGene(int index) {
+        return values[index];
+    }
+
+    @Override
+    public void setGene(int index, Object gene) {
+        values[index] = (Integer)gene;
+    }
+
+    @Override
+    public void Generate() {
+        
+        Random rand = new Random();
+        
+        values = new int[size];
+        for (int i = 0; i < size; i++) {
+            values[i] = rand.nextInt(maxValue);
+        }
+        
+    }
+
+    @Override
+    public IChromosome CreateNew() {
+        return new IntegerChromosome(size, maxValue);
+    }
+
+    @Override
+    public IChromosome Clone() {
+        try{
+            return (IntegerChromosome)super.clone();
+        }
+        catch(CloneNotSupportedException e){
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
+    @Override
+    public int getLength() {
+        return size;
+    }
+    
+}
