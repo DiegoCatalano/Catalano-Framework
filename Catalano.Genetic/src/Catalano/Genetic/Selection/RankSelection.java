c0c5d200667c648233ea42e81c05d2b49397216c
Diego Catalano <diego.catalano@live.com>
2019-05-01 20:59:19 -0300

    Rank Selection
    - New: Rank Selection

--- Catalano.Genetic/src/Catalano/Genetic/Selection/RankSelection.java
+++ Catalano.Genetic/src/Catalano/Genetic/Selection/RankSelection.java
@@ -22,44 +22,61 @@
 
 package Catalano.Genetic.Selection;
 
+import Catalano.Core.ArraysUtil;
 import Catalano.Genetic.Chromosome.IChromosome;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 /**
- *
+ * Rank Selection.
  * @author Diego Catalano
  */
-public class RandomSelection implements ISelection, IRealCodedSelection{
+public class RankSelection implements ISelection{
     
-    private List<ISelection> selections;
+    private long seed;
 
     /**
-     * Initializes a new instance of the ElitismSelection class.
-     * @param selection List of selection operators.
+     * Initializes a new instance of the RankSelection class.
      */
-    public RandomSelection(List<ISelection> selection) {
-        this.selections = selection;
-    }
-
-    @Override
-    public int[] Compute(double[] fitness) {
-        
-        Random rand = new Random();
-        
-        int[] index = new int[2];
-        index[0] = rand.nextInt(fitness.length);
-        index[1] = rand.nextInt(fitness.length);
-        
-        return index;
-        
-    }
+    public RankSelection() {}
 
     @Override
     public int[] Compute(List<IChromosome> chromosomes) {
         
-        Random rand = new Random();
         
-        return selections.get(rand.nextInt(selections.size())).Compute(chromosomes);
+        //Scale fitness based on the rank
+        int[] index = new int[2];
+        double[] fitness = new double[chromosomes.size()];
+        
+        for (int i = 0; i < fitness.length; i++) {
+            fitness[i] = chromosomes.get(i).getFitness();
+        }
+        
+        int[] indexes = ArraysUtil.Argsort(fitness, true);
+        for (int i = 0; i < fitness.length; i++) {
+            fitness[i] = indexes[i] / (double)indexes.length;
+        }
+        
+        //Sort the fitness values
+        Arrays.sort(fitness);
+        
+        //Select the chromossomes
+        Random r = new Random();
+        if(seed != 0) r.setSeed(seed);
+        
+        for (int i = 0; i < index.length; i++) {
+            double v = r.nextDouble();
+            for (int j = 0; j < fitness.length; j++) {
+                if(v >= fitness[j]){
+                    index[i] = indexes[j];
+                }
+            }
+        }
+        
+        return index;
+        
+        
     }
+    
 }
