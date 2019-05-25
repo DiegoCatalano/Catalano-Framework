a53384374ab2c998e9f8c3d8d1a4c69da4cfdd66
Diego Catalano <diego.catalano@live.com>
2019-05-25 19:03:49 -0300

    Updated Blended Crossover
    - Added: Float support in the Blended Crossover.

--- Catalano.Evolutionary/src/Catalano/Evolutionary/Genetic/Crossover/BlendedCrossover.java
+++ Catalano.Evolutionary/src/Catalano/Evolutionary/Genetic/Crossover/BlendedCrossover.java
@@ -23,6 +23,8 @@
 package Catalano.Evolutionary.Genetic.Crossover;
 
 import Catalano.Evolutionary.Genetic.Chromosome.DoubleChromosome;
+import Catalano.Evolutionary.Genetic.Chromosome.FloatChromosome;
+import Catalano.Evolutionary.Genetic.Chromosome.IChromosome;
 import Catalano.Math.Random.Random;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,11 +32,11 @@ import java.util.List;
 /**
  * Blended Crossover.
  * 
- * Support: Double Chromosome.
+ * Support: Double/Float Chromosome.
  * 
  * @author Diego Catalano
  */
-public class BlendedCrossover implements ICrossover<DoubleChromosome>{
+public class BlendedCrossover implements ICrossover<IChromosome>{
     
     private double alpha;
 
@@ -70,8 +72,19 @@ public class BlendedCrossover implements ICrossover<DoubleChromosome>{
     }
 
     @Override
-    public List<DoubleChromosome> Compute(DoubleChromosome chromosome1, DoubleChromosome chromosome2) {
+    public List<IChromosome> Compute(IChromosome chromosome1, IChromosome chromosome2) {
         
+        if(chromosome1 instanceof FloatChromosome){
+            return Compute((FloatChromosome)chromosome1, (FloatChromosome)chromosome2);
+        } else if (chromosome1 instanceof DoubleChromosome){
+            return Compute((DoubleChromosome)chromosome1, (DoubleChromosome)chromosome2);
+        } else{
+            throw new IllegalArgumentException("Blended crossover only works with Double/Float chromosomes.");
+        }
+        
+    }
+    
+    private List<IChromosome> Compute(DoubleChromosome chromosome1, DoubleChromosome chromosome2){
         Random rand = new Random();
         
         DoubleChromosome c1 = (DoubleChromosome)chromosome1.Clone();
@@ -89,11 +102,35 @@ public class BlendedCrossover implements ICrossover<DoubleChromosome>{
             
         }
         
-        List<DoubleChromosome> lst = new ArrayList<>(2);
+        List<IChromosome> lst = new ArrayList<>(2);
         lst.add(c1);
         lst.add(c2);
         
         return lst;
+    }
+    
+    private List<IChromosome> Compute(FloatChromosome chromosome1, FloatChromosome chromosome2){
+        Random rand = new Random();
         
+        FloatChromosome c1 = (FloatChromosome)chromosome1.Clone();
+        FloatChromosome c2 = (FloatChromosome)chromosome2.Clone();
+        
+        int length = c1.getLength();
+        for (int i = 0; i < length; i++) {
+            
+            double min = Math.min((Double)c1.getGene(i), (Double)c2.getGene(i));
+            double max = Math.max((Double)c1.getGene(i), (Double)c2.getGene(i));
+            double delta = max - min;
+            
+            c1.setGene(i, (float)rand.nextDouble(min - alpha*delta, max + alpha*delta));
+            c2.setGene(i, (float)rand.nextDouble(min - alpha*delta, max + alpha*delta));
+            
+        }
+        
+        List<IChromosome> lst = new ArrayList<>(2);
+        lst.add(c1);
+        lst.add(c2);
+        
+        return lst;
     }
 }
